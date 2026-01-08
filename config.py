3e631661b2f12c05db5b98766d0b8a1be751458f
wz7jpprpd8-debug <wz7jpprpd8@privaterelay.appleid.com>
2026-01-08 22:00:41 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not TELEGRAM_TOKEN:
     raise ValueError("TELEGRAM_TOKEN not set")
 
-if not OPENAI_API_KEY:
-    raise ValueError("OPENAI_API_KEY not set")
+AI_ENABLED = bool(OPENAI_API_KEY)
